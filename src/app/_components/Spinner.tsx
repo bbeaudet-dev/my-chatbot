2437edcad63481298a1f66156acb9ee6f72ef79c
Ben Beaudet <bbeaudet0@gmail.com>
2025-06-09 11:18:00 -0400

    created basic chat function

---
+++ src/app/_components/Spinner.tsx
@@ -0,0 +1,5 @@
+export default function Spinner() {
+    return (
+        <p>Totally a spinner</p>
+    )
+}
---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,53 @@
+import { openai } from '@ai-sdk/openai';
+import { streamText } from 'ai';
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  const result = streamText({
+    model: openai('gpt-4-turbo'),
+    system: 'You are a helpful assistant.',
+    messages,
+  });
+
+  return result.toDataStreamResponse();
+}
+
+
+// import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+// import { type NextRequest } from "next/server";
+
+// import { env } from "~/env";
+// import { appRouter } from "~/server/api/root";
+// import { createTRPCContext } from "~/server/api/trpc";
+
+// /**
+//  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
+//  * handling a HTTP request (e.g. when you make requests from Client Components).
+//  */
+// const createContext = async (req: NextRequest) => {
+//   return createTRPCContext({
+//     headers: req.headers,
+//   });
+// };
+
+// const handler = (req: NextRequest) =>
+//   fetchRequestHandler({
+//     endpoint: "/api/trpc",
+//     req,
+//     router: appRouter,
+//     createContext: () => createContext(req),
+//     onError:
+//       env.NODE_ENV === "development"
+//         ? ({ path, error }) => {
+//             console.error(
+//               `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
+//             );
+//           }
+//         : undefined,
+//   });
+
+// export { handler as GET, handler as POST };
