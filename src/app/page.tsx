2001524c606189d77224ade0d03cf20e5e03e028
Ben Beaudet <bbeaudet0@gmail.com>
2025-06-09 17:37:35 -0400

    quick fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
+
 import Chatroom from './_components/Chatroom'
 
-import { redirect } from 'next/navigation'
-import { createChat } from '@tools/chat-store'
-
-export default async function Page() {
-    const id = await createChat()
-    redirect(`/chat/${id}`)
-
-    // return <Chatroom />
+export default function Page() {
+    return <Chatroom />
 }
