8c60589f4a5b984b380490ca7c9282950c69dc10
Ben Beaudet <bbeaudet0@gmail.com>
2025-06-13 15:00:33 -0400

    better-auth installation

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,13 @@
-import { type Config } from "drizzle-kit";
+import { config } from 'dotenv'
+import { defineConfig } from "drizzle-kit"
 
-import { env } from "~/env";
+config( { path: '.env' } )
 
-export default {
-  schema: "./src/server/db/schema.ts",
-  dialect: "postgresql",
-  dbCredentials: {
-    url: env.DATABASE_URL,
-  },
-  tablesFilter: ["my-chatbot_*"],
-} satisfies Config;
+export default defineConfig( {
+     schema: "./src/db/schema.ts",
+     out: "./migrations",
+     dialect: "postgresql",
+     dbCredentials: {
+          url: process.env.DATABASE_URL!,
+     },
+} )
---
+++ src/lib/auth.ts
@@ -0,0 +1,17 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "../db/drizzle"; // your drizzle instance
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth( {
+      emailAndPassword: {
+            enabled: true
+      },
+      socialProviders: {
+            github: {
+                  clientId: process.env.GITHUB_CLIENT_ID as string,
+                  clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+            },
+      },
+      plugins: [nextCookies()]
+} );
